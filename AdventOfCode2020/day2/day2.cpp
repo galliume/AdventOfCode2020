02af28b12eade3a0ab6facf78d6b1844bad821fd
Guillaume P <gcconantc@gmail.com>
2020-12-02 15:16:40 +0100

    Clean

--- AdventOfCode2020/day2/day2.cpp
+++ AdventOfCode2020/day2/day2.cpp
@@ -4,78 +4,40 @@
 
 using namespace std;
 
-int day21()
+int day2()
 {
 	ifstream data;
 	string line;
+	int min = 0, max = 0, count = 0, oldTotal = 0, newTotal = 0;
+	char letter, password[50];
 
 	data.open("day2/input.txt");
 
-	if (data.is_open()) {
-
-		int min = 0, max = 0, count = 0, total = 0;
-		char letter;
-		char password[50];
-
+	if (data.is_open()) 
+	{
 		while (getline(data, line))
 		{	
 			sscanf_s(line.c_str(), "%d-%d %c:%49s", &min, &max, &letter, sizeof(letter), &password, sizeof(password));
 			
 			for (char c : password)
 			{
-				if (c == letter)
-				{
-					count++;
-				}
+				if (c == letter) count++;
 			}
 
-			if (count >= min && count <= max)
-			{
-				total++;
-			}
+			if (count >= min && count <= max) oldTotal++;
+			if ((letter == password[min - 1] && letter != password[max - 1]) || (letter != password[min - 1] && letter == password[max - 1])) newTotal++;
 			
-			cout << min << " - " << max << " - " << letter << " - " << password << endl;	
 			count = 0;
-		}
-
-		cout << "Total " << total;
+		}		
 		data.close();
-	}
+	};
 
-	return 0;
-}
-
-int day22()
-{
-	ifstream data;
-	string line;
-
-	data.open("day2/input.txt");
-
-	if (data.is_open()) {
-
-		int min = 0, max = 0, count = 0, total = 0;
-		char letter;
-		char password[50];
-
-		while (getline(data, line))
-		{
-			sscanf_s(line.c_str(), "%d-%d %c:%49s", &min, &max, &letter, sizeof(letter), &password, sizeof(password));
-			
-			if ((letter == password[min - 1] && letter != password[max - 1]) || (letter != password[min - 1] && letter == password[max - 1]))
-			{
-				total++;
-			}
-		}
-
-		cout << "Total " << total;
-		data.close();
-	}
+	cout << "old : " << oldTotal << " new : " << newTotal << endl;
 
 	return 0;
 }
 
 int main()
 {	
-	return day22();
+	return day2();
 }
